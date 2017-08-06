1abfb164edcd43b33fe8b6c6afe59f6f78485cf2
gostrider <tiddybeardaywalker@hotmail.com>
2017-08-06 16:15:28 +0800

    Updating readme, naming

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# random-sentence-picker
-Random picking sentence from website url
+# sentence-picker
+
+## Description
+`sentence-picker` is a simple tool for random picking text sentence from given url.
+It make use of `spacy` NLP library for returning sentence that most relevant to the title of the webpage.
+
+## Install
+
+Require: Python 3.6+
+```
+pip install -e git+https://github.com/gostrider/random-sentence-picker.git#egg=sentence_picker
+```
+
+## Usage
+```
+from sentence_picker import picker
+
+pass
+```
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='random_sentence',
+    name='sentence_picker',
     url='https://github.com/gostrider/random-sentence-picker',
     description='Random picking sentences from url',
     author='gostrider',
     author_email='tiddybeardaywalker@hotmail.com',
     version='0.1',
-    packages=['random_sentence', ],
+    packages=['sentence_picker', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     install_requires=['beautifulsoup4==4.6.0', 'spacy==1.9.0', 'requests==2.18.3']
