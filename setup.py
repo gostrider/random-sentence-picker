ff279a72c1d9339b53da0251811b184bf50e5fe6
gostrider <tiddybeardaywalker@hotmail.com>
2017-08-06 11:55:56 +0800

    Add setup, Pipfile

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='random-sentence-picker',
+    version='0.1',
+    packages=['random-sentence-picker', ],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
