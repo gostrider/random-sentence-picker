6d65acfa656ccb12606a972b3005edcb77256637
gostrider <tiddybeardaywalker@hotmail.com>
2017-08-06 15:35:39 +0800

    update naming

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='random-sentence-picker',
+    name='random_sentence',
     url='https://github.com/gostrider/random-sentence-picker',
     description='Random picking sentences from url',
     author='gostrider',
     author_email='tiddybeardaywalker@hotmail.com',
     version='0.1',
-    packages=['sentence', ],
+    packages=['random_sentence', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
