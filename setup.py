9b36d01e806c47fd4ef0c4aec71350323b8bb8c4
gostrider <tiddybeardaywalker@hotmail.com>
2017-08-06 15:25:23 +0800

    Update project structure

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from distutils.core import setup
 
 setup(
     name='random-sentence-picker',
+    url='https://github.com/gostrider/random-sentence-picker',
+    description='Random picking sentences from url',
+    author='gostrider',
+    author_email='tiddybeardaywalker@hotmail.com',
     version='0.1',
     packages=['random-sentence-picker', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
